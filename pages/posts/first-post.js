a03b3f60e20f2a70137959344e296b9fc64c548f
rockingatgithub <contactwithsid@gmail.com>
2020-11-12 10:53:37 +0530

    layout added

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+export default function FirstPost() {
+    return <Layout>
+      <Head><title>First Post page</title></Head>
+    <h1>First Post</h1>
+    <Link href="/"><a>Go home</a></Link>
+    </Layout>
+  }
